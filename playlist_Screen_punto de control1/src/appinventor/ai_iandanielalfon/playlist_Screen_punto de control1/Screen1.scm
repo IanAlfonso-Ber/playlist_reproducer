0b912cd1a817244d97ca72506b10f536582a815e
Ian Daniel Alfonso Rodriguez <iandanielalfon@gmail.com>
2025-06-11 07:33:29 +0200

    Delete playlist_Screen_punto de control1 directory

--- playlist_Screen_punto de control1/src/appinventor/ai_iandanielalfon/playlist_Screen_punto de control1/Screen1.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"31","AppName":"playlist","Title":"Screen1","Uuid":"0","Theme":"Classic"}}
-|#
--- playlist_Screen_punto de control1/src/appinventor/ai_iandanielalfon/playlist_Screen_punto de control1/reproductor.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"reproductor","$Type":"Form","$Version":"31","AppName":"playlist","Theme":"Classic","Title":"reproductor","Uuid":"0"}}
-|#
