3a56d0b4d3c8b1d7fc470c66eeb8e6a71194fd1b
Ian Daniel Alfonso Rodriguez <iandanielalfon@gmail.com>
2025-06-11 07:28:35 +0200

    Add files via upload

---
+++ playlist_Screen_punto de control1/src/appinventor/ai_iandanielalfon/playlist_Screen_punto de control1/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"31","AppName":"playlist","Title":"Screen1","Uuid":"0","Theme":"Classic"}}
+|#
---
+++ playlist_Screen_punto de control1/src/appinventor/ai_iandanielalfon/playlist_Screen_punto de control1/reproductor.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"reproductor","$Type":"Form","$Version":"31","AppName":"playlist","Theme":"Classic","Title":"reproductor","Uuid":"0"}}
+|#
